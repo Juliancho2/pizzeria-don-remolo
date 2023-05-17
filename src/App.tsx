1225e6937f86b3066ae3b8cb442f19aadd77a151
julian moreno <julianmoreno242@gmail.com>
2023-05-16 19:57:22 -0500

    removed unused hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
