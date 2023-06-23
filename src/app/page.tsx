ec555feb9735223e4a0cfc07ab0e5af9e42f22ad
julian moreno <julianmoreno242@gmail.com>
2023-06-23 10:24:12 -0500

    Añadio Footer a page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Hero } from '../components/organisms/Hero'
 import { Header } from '../components/organisms/Header'
+import Footer from '../components/organisms/Footer'
 import { Button } from '../components/atomic/Button'
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <main>
         <Hero />
         <Button>Julian</Button>
+        <Footer />
       </main>
     </>
   )
