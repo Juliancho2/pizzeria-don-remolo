93a05e4f434ddd52676a97247c6b12cdcd2d1449
Ulises Vargas <uli.vargas02@gmail.com>
2023-06-24 23:15:49 -0600

    Agrego formulario con Formik en vista inicio de sesion

--- src/pages/admin/signin/index.tsx
+++ src/pages/admin/signin/index.tsx
@@ -1,23 +1,46 @@
+import * as Yup from 'yup'
+import Image from 'next/image'
+import { Form, Formik } from 'formik'
+
+import { TextInput } from '@/src/components/atomic/TextInput'
+import { Logo } from '@/src/components/atomic/Logo'
+import { Button } from '@/src/components/atomic/Button'
+
+const Validations = Yup.object({
+  email: Yup.string().required('Este campo es requerido').email(),
+  password: Yup.string().required('Este campo es requerido')
+})
+
 const SigninPage = () => {
   return (
-    <div className="h-screen">
+    <div className="h-screen w-[90%] mx-auto">
       <div className="flex justify-center items-center h-full">
-        <div className="shadow-md rounded-sm w-[600px] h-[350px] flex flex-col gap-4 p-6 justify-center">
-          <h1 className="text-4xl font-bold text-center mb-10">
-            Inicio de Sesión
-          </h1>
-          <div className="flex flex-col">
-            <label htmlFor="email">Correo: </label>
-            <input type="text" id="email" className="border rounded-sm py-1" />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="password">Contraseña: </label>
-            <input
-              type="text"
-              id="password"
-              className="border rounded-sm py-1"
-            />
-          </div>
+        <div className="shadow-md rounded-sm w-[500px] h-[500px] flex flex-col p-10 items-center justify-between">
+          <Logo widht={200} height={200} />
+          <Formik
+            initialValues={{
+              email: '',
+              password: ''
+            }}
+            validationSchema={Validations}
+            onSubmit={(values: unknown) => console.log(values)}
+          >
+            <Form className="w-full">
+              <div className="flex flex-col gap-5">
+                <TextInput
+                  name="email"
+                  type="email"
+                  placeholder="Correo electrónico"
+                />
+                <TextInput
+                  name="password"
+                  type="password"
+                  placeholder="Contraseña"
+                />
+                <Button styles="btn">Iniciar sesión</Button>
+              </div>
+            </Form>
+          </Formik>
         </div>
       </div>
     </div>
