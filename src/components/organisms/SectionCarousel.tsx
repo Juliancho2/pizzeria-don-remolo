884d03ca9fcb32b3ef1113cf1a1b5ad6cb4d7a93
julian moreno <julianmoreno242@gmail.com>
2023-06-28 10:55:08 -0500

    Se agrego padding bottom y top SectionCarousel

--- src/components/organisms/SectionCarousel.tsx
+++ src/components/organisms/SectionCarousel.tsx
@@ -4,7 +4,7 @@ import Carousel from '../molecules/Carousel'
 
 const SectionCarousel = () => {
   return (
-    <section className="min-h-[50vh]  flex items-center w-full">
+    <section className="min-h-[50vh] py-20  flex items-center w-full">
       <div className="flex overflow-hidden sm:w-[90%] mx-auto max-w-[1200px] gap-5 flex-col   px-1 py-5 w-full justify-center">
         <h2 className="font-bold text-2xl sm:text-3xl px-2">
           ESCOGE TU MASA FAVORITA
